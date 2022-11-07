cdd9ecd47c10e547f358f828bf850d5df6179eb1
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2022-11-07 13:26:20 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# red_mad_robot Android Docker Images
+
+Set of docker images used by red_mad_robot Android team.
+All images are published in [GitHub Container Registry][ghcr].
+
+---
+
+## android-sdk
+
+### android-sdk:base
+
+> `ghcr.io/redmadrobot/android/android-sdk:base`
+
+Base Android image. All other android images are built on top of this image.
+
+**Base image**: `eclipse-temurin:11-jdk-jammy` \
+**Packages:**
+
+- sdkmanager:
+    - cmdline-tools;latest
+    - platform-tools
+    - build-tools;**30.0.3**
+    - extras;android;m2repository
+    - extras;google;google_play_services
+    - extras;google;m2repository"
+- python3
+- git
+- zip, unzip
+
+### android-sdk:[x]
+
+> `ghcr.io/redmadrobot/android/android-sdk:[x]`
+
+Where `[x]` is one of API levels:
+
+- 33 (Android 13)
+- 32 (Android 12L)
+- 31 (Android 12)
+- 30 (Android 11)
+
+### android-sdk:[x]-ndk
+
+> `ghcr.io/redmadrobot/android/android-sdk:[x]-ndk` \
+> `ghcr.io/redmadrobot/android/android-sdk:[x]-ndk-[y]`
+
+Where `[x]` is SDK version and `[y]` is NDK version (optionally).
+
+**NDK version**: 25.1.8937393
+
+<details>
+<summary>Other images</summary>
+
+    android-sdk:32-ndk-22.1.7171670
+    android-sdk:31-ndk-22.1.7171670
+    android-sdk:30-ndk-22.1.7171670
+
+</details>
+
+## android-emu:[x]
+
+> `ghcr.io/redmadrobot/android/android-emu:30`
+
+:warning: Experimental image to run android emulator on CI.
+
+**Base image**: `android-sdk:30`
+
+To start emulator, you should run script `/start_emulator.sh`.
+Emulator will be named as **EMU_X**, where **X** - is an SDK version (for, API **30** name will be **EMU_30**).
+
+## ruby:[x]
+
+> `ghcr.io/redmadrobot/android/ruby:3.0` \
+> `ghcr.io/redmadrobot/android/ruby:2.7`
+
+Ruby image with some additions to work with Fastlane and Danger.
+
+**Base image:** `ruby:[x]-slim` \
+**Packages:**
+
+- Bundler
+- Firebase CLI
+
+## danger-kotlin:[x]
+
+> `ghcr.io/redmadrobot/android/danger-kotlin:1.0.0` \
+> `ghcr.io/redmadrobot/android/danger-kotlin:1.1.0`
+
+[Danger-kotlin][danger-kotlin] docker image.
+
+- **gradle**: 5.6.2
+- **jdk**: 8
+- **kotlin-compiler**: 1.5.0
+- **danger-kotlin**: [x]
+
+## License
+
+[MIT](LICENSE)
+
+[registry]: https://git.redmadrobot.com/DevOps/docker-android-builder/container_registry
+
+[ghcr]: https://github.com/orgs/RedMadRobot/packages?ecosystem=container&q=android%2F
+
+[danger-kotlin]: https://github.com/danger/kotlin
---
+++ android-emu/start_emulator.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+RESET="\033[0m"
+F_BOLD="\033[1m"
+F_BOLD_RESET="\033[22m"
+C_RED="\033[31m"
+TIMEOUT_IN_SEC=360
+
+function log_info() { printf -- "%s\n" "$*"; }
+function log_success() { printf -- "\n${F_BOLD}${C_GREEN}%s${RESET}\n" "$*"; }
+function log_error() { printf -- "\n${F_BOLD}${C_RED}ERROR:${F_BOLD_RESET} %s${RESET}\n" "$*"; }
+
+function fail_with_msg() { 
+    printf -- "\n${F_BOLD}${C_RED}ERROR:${F_BOLD_RESET} %s${RESET}\n" "$*"; 
+    exit 1
+}
+
+function check_kvm() {
+    cpu_support_hardware_acceleration=$(grep -cw ".*\(vmx\|svm\).*" /proc/cpuinfo)
+    if [ "$cpu_support_hardware_acceleration" != 0 ]; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+function config_emulator_settings() {
+    log_info "Let's configure emulator via adb.."
+    adb root
+    adb shell "settings put global window_animation_scale 0.0"
+    adb shell "settings put global transition_animation_scale 0.0"
+    adb shell "settings put global animator_duration_scale 0.0"
+    adb shell "settings put secure spell_checker_enabled 0"
+    adb shell "settings put secure show_ime_with_hard_keyboard 1"
+    adb shell "settings put system screen_off_timeout 1800000"
+    adb shell "settings put secure long_press_timeout 1500"
+    adb shell "am broadcast -a com.android.intent.action.SET_LOCALE --es com.android.intent.extra.LOCALE EN"
+    adb shell input keyevent 82
+}
+
+function wait_emulator() {
+    log_info "Wait device..."
+    timeout $TIMEOUT_IN_SEC adb wait-for-device && log_success "Device connected!" || \
+        fail_with_msg "Timeout ($TIMEOUT_IN_SEC seconds) reached - failed to wait the device"
+}
+
+function wait_emulator_boot_completed() {
+    failcounter=0
+
+    log_info "Wait when boot is fully completed..."
+
+    while [ true ]; do
+        status=$(adb shell getprop sys.boot_completed | tr -d '\r')
+
+        if [ "$status" == "1" ]; then
+            log_success "Boot completed!"
+            break
+        else
+            let "failcounter += 1"
+
+            if [[ $failcounter -gt TIMEOUT_IN_SEC ]]; then
+                fail_with_msg "Timeout ($TIMEOUT_IN_SEC seconds) reached - failed to start emulator"
+            fi
+
+            sleep 1
+            log_info "try #$failcounter/$TIMEOUT_IN_SEC, boot in progress, still waiting..."
+        fi
+    done
+}
+
+function wait_emulator_to_be_ready() {
+    adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
+
+    ${ANDROID_HOME}/emulator/emulator -avd ${EMULATOR_NAME} -verbose -no-boot-anim -wipe-data -no-snapshot -no-audio -no-window &
+
+    wait_emulator
+    wait_emulator_boot_completed
+}
+
+function start_emulator_if_possible() {
+    log_info "Let's try to run the emulator"
+
+    check_kvm=$(check_kvm)
+
+    if [ "$check_kvm" != "1" ]; then
+        log_error "Run emulator failed, nested virtualization is not supported"
+        exit 1
+    else
+        wait_emulator_to_be_ready
+        sleep 1
+        config_emulator_settings
+    fi
+}
+
+start_emulator_if_possible
